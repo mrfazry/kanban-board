de5ce28ddcdb068255781188429fe8bb559f590b
mrfazry <mredykafazry@gmail.com>
2018-12-23 12:47:32 +0700

    Updates

---
+++ precache-manifest.27d5469dc3dd134b17184d62222437c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ee7fcb932540041e18ee4e034a14a89",
+    "url": "/kanban-board/static/media/spinner.8ee7fcb9.gif"
+  },
+  {
+    "revision": "96213ff335c6a139d385",
+    "url": "/kanban-board/static/js/runtime~main.96213ff3.js"
+  },
+  {
+    "revision": "e7987b7d534927815f6d",
+    "url": "/kanban-board/static/js/main.e7987b7d.chunk.js"
+  },
+  {
+    "revision": "7362c9aff3f9a8834e6a",
+    "url": "/kanban-board/static/js/1.7362c9af.chunk.js"
+  },
+  {
+    "revision": "e7987b7d534927815f6d",
+    "url": "/kanban-board/static/css/main.86d3da89.chunk.css"
+  },
+  {
+    "revision": "e413894d10d9b27bbae735c0de517e6e",
+    "url": "/kanban-board/index.html"
+  }
+];
